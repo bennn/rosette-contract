09839a5c86f8166f98c795c29aec3bfb025feafd
Ben Greenman <benjaminlgreenman@gmail.com>
2016-12-04 04:58:31 -0500

    [test] ackerman verified, sort of
    actually takes too long to run the solver on the normal ackermann function

---
+++ test/shootout/README.md
@@ -0,0 +1,10 @@
+shootout
+===
+
+Tests from the shootout benchmarks.
+
+Online here:
+https://github.com/racket/racket/tree/master/pkgs/racket-benchmarks/tests/racket/benchmarks/shootout
+
+Some already-contracted benchmarks here:
+https://github.com/LeifAndersen/experimental-methods-in-pl/tree/master/shootout
---
+++ test/shootout/ackermann.rkt
@@ -0,0 +1,18 @@
+#lang racket/base
+
+(require
+  rosette-contract
+  ;racket/contract
+)
+
+(define/contract (ack m n)
+  (-> exact-nonnegative-integer? exact-nonnegative-integer? exact-nonnegative-integer?)
+  (cond
+   ((zero? m) (+ n 1))
+   ((zero? n) (ack (- m 1) 1))
+   (else      (ack 0 #;(- m 1) (ack 0 #;m (- n 1))))))
+
+(define (main)
+  (ack 3 3))
+
+(time (begin (main) (void)))
